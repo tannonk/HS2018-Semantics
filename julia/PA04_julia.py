f602962af9239c1dd803339ca060a0d0dd67a41b
yunigma <julia.nigmatic@yandex.ru>
2018-12-04 17:47:04 +0100

    added .py version of the code

---
+++ julia/PA04_julia.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# SA, HS18
+# PA04
+
+
+import gzip
+import numpy as np
+import random
+import os
+import json
+from scipy.sparse import csr_matrix
+
+from collections import Counter, defaultdict, namedtuple
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_recall_fscore_support, fbeta_score, make_scorer
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_score, StratifiedKFold, KFold
+from sklearn.preprocessing import FunctionTransformer,LabelEncoder
+
+
+f_scorer = make_scorer(fbeta_score, beta=0.5, average='macro')
+
+# LOAD THE DATA
+PairExample = namedtuple('PairExample',
+                         'entity_1, entity_2, snippet')
+Snippet = namedtuple('Snippet',
+                     'left, mention_1, middle, mention_2, right, direction')
+
+
+def load_data(file, verbose=False):
+    f = open(file, 'r', encoding='utf-8')
+    data = []
+    labels = []
+    for i, line in enumerate(f):
+        instance = json.loads(line)
+        if i == 0:
+            if verbose:
+                print('json example:')
+                print(instance)
+        # 'relation, entity_1, entity_2, snippet' fileds for each example
+        # 'left, mention_1, middle, mention_2, right, direction' for each snippet
+        instance_tuple = PairExample(instance['entity_1'], instance['entity_2'], [])
+        for snippet in instance['snippet']:
+            try:
+                snippet_tuple = Snippet(snippet['left'], snippet['mention_1'],
+                                        snippet['middle'],
+                                        snippet['mention_2'], snippet['right'],
+                                        snippet['direction'])
+                instance_tuple.snippet.append(snippet_tuple)
+            except:
+                print(instance)
+        if i == 0:
+            if verbose:
+                print('\nexample transformed as a named tuple:')
+                print(instance_tuple)
+        data.append(instance_tuple)
+        labels.append(instance['relation'])
+
+    return data, labels
+
+
+# Statistics over relations
+def print_stats(labels):
+    labels_counts = Counter(labels)
+    print('{:20s} {:>10s} {:>10s}'.format('', '', 'rel_examples'))
+    print('{:20s} {:>10s} {:>10s}'.format('relation', 'examples',
+                                          '/all_examples'))
+    print('{:20s} {:>10s} {:>10s}'.format('--------', '--------', '-------'))
+
+    for k,v in labels_counts.items():
+        print('{:20s} {:10d} {:10.2f}'.format(k, v, v / len(labels)))
+    print('{:20s} {:>10s} {:>10s}'.format('--------', '--------', '-------'))
+    print('{:20s} {:10d} {:10.2f}'.format('Total', len(labels),
+                                          len(labels) / len(labels)))
+
+
+# get full context
+def get_context(data):
+    all_data = []
+    for instance in data:
+        s_context = []
+        for s in instance.snippet:
+            s_context.append(' '.join((s.left, s.mention_1, s.middle,
+                                       s.mention_2, s.right)))
+            # s_context.append(' '.join((s.left, s.middle, s.right)))
+        all_data.append(' '.join(s_context))
+
+    return all_data
+
+
+# Extract two simple features
+def ExractSimpleFeatures(data, verbose=False):
+    featurized_data = []
+    for instance in data:
+        featurized_instance = {
+            'mid_words': '',
+            'distance': np.inf,
+            'left': [],
+            'right': [],
+            'mid': []
+        }
+        for s in instance.snippet:
+            if len(s.middle.split()) < featurized_instance['distance']:
+                featurized_instance['mid_words'] = s.middle
+                featurized_instance['distance'] = len(s.middle.split())
+            featurized_instance['left'] = s.left
+            featurized_instance['right'] = s.right
+            featurized_instance['mid'] = s.middle
+            # context = [s.left + s.right + s.middle]
+            # vec_context = vectorizer.transform(context)
+            # featurized_instance['left'] = vectorizer.transform([s.left])
+            # featurized_instance['right'] = vectorizer.transform([s.right])
+            # featurized_instance['mid'] = vectorizer.transform([s.middle])
+        featurized_data.append(featurized_instance)
+    if verbose:
+        print(len(data))
+        print(len(featurized_data))
+        print(data[0])
+        print(featurized_data[0])
+
+    return featurized_data
+
+
+def print_statistics_header():
+    print('{:20s} {:>10s} {:>10s} {:>10s} {:>10s}'.format(
+        'relation', 'precision', 'recall', 'f-score', 'support'))
+    print('{:20s} {:>10s} {:>10s} {:>10s} {:>10s}'.format(
+        '-' * 18, '-' * 9, '-' * 9, '-' * 9, '-' * 9))
+
+
+def print_statistics_row(rel, result):
+    print('{:20s} {:10.3f} {:10.3f} {:10.3f} {:10d}'.format(rel, *result))
+
+
+def print_statistics_footer(avg_result):
+    print('{:20s} {:>10s} {:>10s} {:>10s} {:>10s}'.format(
+        '-' * 18, '-' * 9, '-' * 9, '-' * 9, '-' * 9))
+    print('{:20s} {:10.3f} {:10.3f} {:10.3f} {:10d}'.format('macro-average',
+                                                            *avg_result))
+
+
+def macro_average_results(results):
+    avg_result = [np.average([r[i] for r in results.values()]) for i in range(3)]
+    avg_result.append(np.sum([r[3] for r in results.values()]))
+    return avg_result
+
+
+def average_results(results):
+    avg_result = [np.average([r[i] for r in results]) for i in range(3)]
+    avg_result.append(np.sum([r[3] for r in results]))
+    return avg_result
+
+
+def evaluateCV(classifier, label_encoder, X, y, verbose=True):
+    results = {}
+    for rel in label_encoder.classes_:
+        results[rel] = []
+    if verbose:
+        print_statistics_header()
+        kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+        for train_index, test_index in kfold.split(X, y):
+            # print("TRAIN:", train_index, "TEST:", test_index)
+            X_train, X_test = [X[i] for i in train_index], [X[i] for i in test_index]
+            y_train, y_test = [y[i] for i in train_index], [y[i] for i in test_index]
+            classifier.fit(X_train, y_train)
+            pred_labels = classifier.predict(X_test)
+            stats = precision_recall_fscore_support(y_test, pred_labels, beta=0.5)
+            # print(stats)
+            for rel in label_encoder.classes_:
+                rel_id = label_encoder.transform([rel])[0]
+            # print(rel_id,rel)
+                stats_rel = [stat[rel_id] for stat in stats]
+                results[rel].append(stats_rel)
+        for rel in label_encoder.classes_:
+            results[rel] = average_results(results[rel])
+            if verbose:
+                print_statistics_row(rel, results[rel])
+    avg_result = macro_average_results(results)
+    if verbose:
+        print_statistics_footer(avg_result)
+    return avg_result[2]  # return f_0.5 score as summary statistic
+
+
+def evaluateCV_check(classifier, X, y, verbose=True):
+    kfold = StratifiedKFold(n_splits = 5, shuffle=True, random_state=0)
+    scores = cross_val_score(classifier, X, y, cv=kfold, scoring=f_scorer)
+    print("\nCross-validation scores (StratifiedKFold): ", scores)
+    print("Mean cv score (StratifiedKFold): ", scores.mean())
+
+
+def main():
+    train_data, train_labels = load_data('data/train.json.txt')
+    print('Train set statistics:')
+    print_stats(train_labels)
+    vectorizer = CountVectorizer()
+    test_data, test_labels = load_data('data/test.json.txt', verbose=False)
+
+    all_train = get_context(train_data)
+    all_test = get_context(test_data)
+
+    vectorizer.fit(all_train)
+    # DATA Count vector
+    train_CV_vectorized = vectorizer.transform(all_train).toarray()
+    test_CV_vectorized = vectorizer.transform(all_test).toarray()
+
+    # DATA ExractSimpleFeatures
+    # train_simple_featurized = ExractSimpleFeatures(train_data, verbose=False)
+    # test_simple_featurized = ExractSimpleFeatures(test_data, verbose=False)
+    # Transform labels to nimeric values
+
+    le = LabelEncoder()
+    train_labels_featurized = le.fit_transform(train_labels)
+
+    # Fit model one vs rest logistic regression
+    # clf = make_pipeline(DictVectorizer(), LogisticRegression())
+
+    # if with CountVectorizer
+    clf = LogisticRegression()
+
+    # CV
+    evaluateCV(clf, le, train_CV_vectorized, train_labels_featurized)
+    evaluateCV_check(clf, train_CV_vectorized, train_labels_featurized)
+
+    # TEST data
+    # Fit final model on the full train data
+    clf.fit(train_CV_vectorized, train_labels_featurized)
+
+    # Predict on test set
+    test_label_predicted = clf.predict(test_CV_vectorized)
+
+    # Deprecation warning explained: https://stackoverflow.com/questions/49545947/sklearn-deprecationwarning-truth-value-of-an-array
+    test_label_predicted_decoded = le.inverse_transform(test_label_predicted)
+    print(test_label_predicted_decoded[:5])
+    f = open("test_labels.txt", 'w', encoding="utf-8")
+    for label in test_label_predicted_decoded:
+        f.write(label + '\n')
+
+
+if __name__ == "__main__":
+    main()
